6fd0c0efb9f0107a19d6cfbdf1a7ee82cef71696
Sergei Dorogin <segy@google.com>
2023-05-01 17:06:56 +0400

    ARP-in-GCE

--- gcp/cloud-run-button/prebuild.sh
+++ gcp/cloud-run-button/prebuild.sh
@@ -54,17 +54,17 @@ gcloud services enable eventarc.googleapis.com
 gcloud services enable googleads.googleapis.com
 gcloud services enable cloudscheduler.googleapis.com
 
-echo -e "${CYAN}Creating Python virtual environment...${NC}"
+echo -e "${CYAN}Creating Python virtual environment...${WHITE}"
 python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
-echo -e "${CYAN}Generating App Reporting pack configuration...${NC}"
+echo -e "${CYAN}Generating App Reporting pack configuration...${WHITE}"
 # generate ARP config and optionally google-ads.yaml (if it wasn't uploaded in cloud shell vm)
 RUNNING_IN_GCE=true
 export RUNNING_IN_GCE   # signaling to run-local.sh that we're runnign inside GCE (there'll be less questions)
 ./run-local.sh --generate-config-only
 
-echo -e "${CYAN}Deploying Cloud components...${NC}"
+echo -e "${CYAN}Deploying Cloud components...${WHITE}"
 # deploy solution
 ./gcp/setup.sh deploy_public_index deploy_all start
